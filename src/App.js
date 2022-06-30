cae27aac567002a3c64951bdb3d446931a53bd68
D-Ram1 <104790915+D-Ram1@users.noreply.github.com>
2022-06-30 13:41:04 -0700

    Updated coding in App.js
    fixed syntax error.

--- src/App.js
+++ src/App.js
@@ -40,9 +40,9 @@ class App extends Component {
     }})
   }
 
-  ccalculateFaceLocation = (data) => {
+  calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputimage');
+    const image = document.getElementById('inputImage');
     const width = Number(image.width);
     const height = Number(image.height);
     return {
